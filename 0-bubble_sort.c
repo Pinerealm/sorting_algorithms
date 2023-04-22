0776efc436f2b169e63ca05cca4d3567adf4a646
Pinerealm <osalami09@gmail.com>
2023-04-22 14:27:57 +0300

    Change second function parameter name

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,20 +15,20 @@ void swap(int *a, int *b)
 /**
  *bubble_sort - a function that sorts elements of an array in ascending order
  *@array: array
- *@n: size of the array
+ *@size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < n - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				print_array(array, n);
+				print_array(array, size);
 			}
 		}
 	}
