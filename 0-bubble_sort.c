c90c0d82c0dbf871330565dd784cecfeb920fe4b
Pinerealm <osalami09@gmail.com>
2025-12-24 21:11:06 +0300

    Refactor bubble_sort: remove swap function and optimize loop conditions

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,34 @@
 #include "sort.h"
-/**
- *swap - a function that swaps two integers
- *@a: first integer
- *@b: second integer
- */
-void swap(int *a, int *b)
-{
-	int temp;
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
 /**
- *bubble_sort - a function that sorts elements of an array in ascending order
- *@array: array
- *@size: size of the array
+ * bubble_sort - a function that sorts elements of an array in ascending order
+ * @array: array
+ * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
 
-	for (i = 0; i < size; i++)
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		if (!swapped)
+			break;
 	}
 }
